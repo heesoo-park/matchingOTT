3f69710afe0ca96e81bc0b32f299cd3eb0585155
김인엽 <wintiger98@gmail.com>
2021-12-11 01:20:33 +0900

    탈퇴까지 완료

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GroupView, DetailGroupView, selectOTT, getGroup
+from .views import GroupView, DetailGroupView, selectOTT, getGroup, groupOut, index
 
 urlpatterns = [
     path('group/<int:id>/', DetailGroupView.as_view()),
     path('group/', GroupView.as_view()),
     path('select/', selectOTT.as_view()),
+    path('index/', index),
     path('getGroupMember/', getGroup),
-    #path('groupOut/', groupOut),
+    path('groupOut/', groupOut),
 ]
